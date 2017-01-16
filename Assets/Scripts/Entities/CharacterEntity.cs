38302ee7c4fa8b3ef6b39bca6e0c4b01277ae54f
ETU\r.maurouard <r.maurouard@rubika-edu.com>
2017-01-16 11:42:01 +0100

    Added player & character entity

---
+++ Assets/Scripts/Entities/CharacterEntity.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using DG.Tweening;
+public abstract class CharacterEntity : MonoBehaviour, IDamageable
+{
+
+    [Header("Life")]
+    public float maxLife;
+    public float currentLife;
+
+    [Header("Attack")]
+    public float attackDamage;
+    public float attackCooldown;
+
+    [Header("Move")]
+    public float MoveSpeed;
+    public bool canMove = true;
+
+    [Header("Invulnerability")]
+    public float timerHitMax;
+    public float timerHit;
+
+    protected enum MoveState //enum for animations
+    {
+        Up,
+        Down,
+        Right,
+        Left,
+    }
+
+    protected MoveState currentMoveState;
+
+    private bool canTakeDamage = false;
+
+    //******** Getter **************
+    public float CurrentLife
+    {
+        get { return currentLife; }
+    }
+
+    //******** Getter / Setter **************
+    public bool CanTakeDamage
+    {
+        get
+        {
+            return canTakeDamage;
+        }
+
+        set
+        {
+            canTakeDamage = value;
+        }
+    }
+
+    public virtual void Start()
+    {
+        currentLife = maxLife;
+    }
+
+    public virtual void Update()
+    {
+        if (timerHit > 0)
+        {
+            timerHit -= Time.deltaTime;
+        }
+    }
+
+    public virtual void TakeDamage(float damageTaken)
+    {
+        if (timerHit <= 0)
+        {
+            currentLife -= damageTaken;
+            currentLife = Mathf.Max(0, CurrentLife);
+            StopCoroutine(GetHitFeedBack());
+            StartCoroutine(GetHitFeedBack());
+            if (currentLife <= 0)
+            {
+                Death();
+            }
+            timerHit = timerHitMax;
+        }
+    }
+
+    public virtual void HealDamage(float healTaken)
+    {
+        currentLife += healTaken;
+        currentLife = Mathf.Min(maxLife, CurrentLife);
+    }
+
+    public abstract void Death();
+
+    public abstract void Move();
+    public abstract void Attack();
+
+    //Return if the current entity is dead or not
+    public bool IsDead()
+    {
+        if (currentLife <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public IEnumerator GetHitFeedBack()
+    {
+        if (GetComponentInChildren<SpriteRenderer>())
+        {
+            SpriteRenderer sp = GetComponentInChildren<SpriteRenderer>();
+            Color initialColor = Color.white;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sp.DOColor(Color.red, timerHitMax / 6);
+                }
+                else
+                {
+                    sp.DOColor(initialColor, timerHitMax / 6);
+                }
+                yield return new WaitForSeconds(timerHitMax / 6);
+            }
+            sp.DOKill(true);
+            sp.color = initialColor;
+        }
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : CharacterEntity {
+
+    public GameObject lifeContainer;
+    public bool isMoving;
+    Animator animator;
+    SpriteRenderer SprRend;
+    // Use this for initialization
+    public override void Start()
+    {
+        base.Start();
+        animator = GetComponentInChildren<Animator>();
+        SprRend = GetComponentInChildren<SpriteRenderer>();
+        canMove = true;
+        currentMoveState = MoveState.Down;
+    }
+
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        Move();
+    }
+
+    public override void Move()
+    {
+        Vector3 move = Vector3.zero;
+        canMove = true;
+        isMoving = false;
+
+        if (canMove)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                move.z = 1;
+                currentMoveState = MoveState.Up;
+                isMoving = true;
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                move.x = -1;
+                currentMoveState = MoveState.Right;
+                isMoving = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                move.z = -1;
+                currentMoveState = MoveState.Down;
+                isMoving = true;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                move.x = 1;
+                currentMoveState = MoveState.Left;
+                isMoving = true;
+            }
+
+            if (isMoving)
+            {
+                animator.speed = 1;
+                switch (currentMoveState)
+                {
+                    case MoveState.Up:
+                        animator.SetBool("goUp", true);
+                        animator.SetBool("goLeft", false);
+                        animator.SetBool("goRight", false);
+                        SprRend.flipX = false;
+                        break;
+                    case MoveState.Down:
+                        animator.SetBool("goUp", false);
+                        animator.SetBool("goLeft", false);
+                        animator.SetBool("goRight", false);
+                        SprRend.flipX = false;
+                        break;
+                    case MoveState.Right:
+                        animator.SetBool("goRight", false);
+                        animator.SetBool("goLeft", true);
+                        SprRend.flipX = true;
+                        break;
+                    case MoveState.Left:
+                        animator.SetBool("goLeft", false);
+                        animator.SetBool("goRight", true);
+                        SprRend.flipX = false;
+                        break;
+                }
+                GetComponent<Rigidbody>().MovePosition(transform.position + move.normalized * MoveSpeed * Time.deltaTime);
+                //this.gameObject.transform.position += move.normalized * MoveSpeed * Time.deltaTime;
+            }
+            else
+            {
+                animator.speed = 0;
+            }
+        }
+        else
+        {
+            animator.speed = 1;
+        }
+    }
+
+    public override void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Death()
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
